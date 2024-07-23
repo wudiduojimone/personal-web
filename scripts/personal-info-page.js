1dbc443cf547a0d4fca6c6d19812338eee2246b1
磊 张 <826334496@qq.com>
2024-07-23 11:28:28 +0800

    fix：碰撞小球增加恶魔圈

--- scripts/personal-info-page.js
+++ scripts/personal-info-page.js
@@ -5,6 +5,8 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = "300");
 
+const luckyBallNumField = document.querySelector(".p-ball-num");
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -21,13 +23,18 @@ function randomColor() {
   );
 }
 
-function Ball(x, y, velX, velY, color, size) {
+function Shape(x, y) {
   this.x = x;
   this.y = y;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  Shape.call(this, x, y);
   this.velX = velX;
   this.velY = velY;
   this.color = color;
   this.size = size;
+  this.exist = true;
 }
 
 Ball.prototype.draw = function () {
@@ -60,7 +67,7 @@ Ball.prototype.update = function () {
 
 Ball.prototype.collisionDetect = function () {
   for (let j = 0; j < balls.length; j++) {
-    if (this !== balls[j]) {
+    if (balls[j].exist&&(this !== balls[j])) {
       const dx = this.x - balls[j].x;
       const dy = this.y - balls[j].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
@@ -72,12 +79,37 @@ Ball.prototype.collisionDetect = function () {
   }
 };
 
-let balls = [];
+function EvilCircle(x, y) {
+  Shape.call(this, x, y);
+  this.color = "black";
+  this.size = 20;
+}
 
-while (balls.length < 25) {
+EvilCircle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+EvilCircle.prototype.control = function () {
+  for (let j = 0; j < balls.length; j++) {
+    const dx = this.x - balls[j].x;
+    const dy = this.y - balls[j].y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    if (balls[j].exist&&(distance < this.size + balls[j].size)) {
+      balls[j].exist = false;
+      luckyBallNum--;
+      luckyBallNumField.textContent = "剩余幸运小球数量为： " + String(luckyBallNum);
+    }
+  }
+};
+
+let luckyBallNum = 25;
+let balls = [];
+while (balls.length < luckyBallNum) {
   let size = random(10, 20);
   let ball = new Ball(
-    // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -85,18 +117,28 @@ while (balls.length < 25) {
     randomColor(),
     size
   );
+  if (ball.velX === 0 && ball.velY === 0) {
+    ball.velX = 2;
+  }
   balls.push(ball);
 }
 
+let circle = new EvilCircle(random(0, width), random(0, height));
+
 function loop() {
-  ctx.fillStyle = "#fff";//"rgba(0, 0, 0, 0.25)";
+  ctx.fillStyle = "#fff"; //"rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
+  circle.draw();
 
   for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].exist) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
   }
+  circle.control();
+
 
   requestAnimationFrame(loop);
 }
